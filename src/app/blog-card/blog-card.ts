7ffd45424163c5f8047eb7f9cf286e0ee83ba2f8
fabio <fabiopalma2504@gmail.com>
2026-06-17 22:05:17 +0200

    chore: fixed typo's and deleted tests

--- src/app/blog-card/blog-card.ts
+++ src/app/blog-card/blog-card.ts
@@ -2,11 +2,11 @@ import { Component, input, output } from '@angular/core';
 import { Blog } from '../models/blog.model';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatIcon } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-blog-card',
-  imports: [MatCardModule, MatButtonModule, MatIcon],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './blog-card.html',
   styleUrl: './blog-card.scss',
 })
