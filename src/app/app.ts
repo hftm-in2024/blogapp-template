a2c81af0cd324a6a17e883821a0783240867440b
fabio <fabiopalma2504@gmail.com>
2026-06-26 17:28:35 +0200

    feat: added custom dark palette and theme colors for dark mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = 'HFTM Web Applications (IN353)';
+
+  protected isDarkMode = false;
+
+  protected toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.documentElement.classList.toggle('dark-theme', this.isDarkMode);
+  }
 }
