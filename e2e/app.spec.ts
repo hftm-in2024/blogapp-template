8f9bfe2aeecfdde37098c56290c07efab24285db
fabio <fabiopalma2504@gmail.com>
2026-08-10 20:37:46 +0200

    fix: fixed tests to pass playwright

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -3,8 +3,6 @@ import { expect, test } from '@playwright/test';
 test('should display the blog overview page', async ({ page }) => {
   await page.goto('/');
 
-  const firstBlogCard = page.locator('app-blog-card').first();
-
-  await expect(firstBlogCard).toBeVisible();
-  await expect(firstBlogCard).toContainText('Angular Signals');
+  await expect(page.locator('app-blog-card').first()).toBeVisible();
+  await expect(page.locator('app-blog-card').first()).toContainText('Angular Signals');
 });
